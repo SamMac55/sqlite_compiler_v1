187b93e945273f7d09c210ae35b1a4bc26188165
SamMac55 <samanthamac055@gmail.com>
2026-05-04 10:24:10 -0500

    update hr test cases to reflect count * change

--- output/expected_output_hr_select/output.sql
+++ output/expected_output_hr_select/output.sql
@@ -5,3 +5,4 @@ SELECT employees.first_name, employees.last_name, employee_projects.emp_role FRO
 SELECT employees.first_name, employees.last_name, employee_projects.emp_role, projects.project_name FROM employees JOIN employee_projects ON employee_projects.employee_id = employees.employee_id JOIN projects ON projects.project_id = employee_projects.project_id ;
 SELECT projects.project_name FROM projects WHERE budget <= 45000;
 SELECT employees.first_name, employees.last_name, employees.salary FROM employees WHERE department_id > 1 ORDER BY salary ASC LIMIT 1;
+SELECT employees.department_id, COUNT(*), departments.dept_name FROM employees JOIN departments ON departments.department_id = employees.department_id  GROUP BY employees.department_id, dept_name;
--- output/output.sql
+++
@@ -1,1 +0,0 @@
-SELECT employees.department_id, COUNT(*), departments.dept_name FROM employees JOIN departments ON departments.department_id = employees.department_id  GROUP BY employees.department_id, dept_name;
